69b8256d844f729eb60f4ebaeedda48f546e36c0
akikan-alumi <y_r9fj22sdf@live.jp>
2015-08-24 00:17:49 +0900

    first commit

---
+++ Assets/main/moveUV.cs
@@ -0,0 +1,28 @@
+﻿/*
+ ステージのUVを動かして、さも動いているように見せるクラス
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class moveUV : MonoBehaviour
+{
+    public GameObject gameObject;
+    private Renderer rend;
+
+    private float vecx = 0;
+    private float vecy = 0;
+
+    void Start()
+    {
+        //レンダラーからマテリアルにアクセスしないとうまく動かない模様　おのれUnity5
+        this.rend = gameObject.GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        vecx = (vecx < 1) ? (vecx + 0.01f) : 0f;
+        vecy = (vecy < 1) ? (vecy + 0.01f) : 0f;
+        this.rend.material.SetTextureOffset("_MainTex", new Vector2(vecx, vecy));
+    }
+}
